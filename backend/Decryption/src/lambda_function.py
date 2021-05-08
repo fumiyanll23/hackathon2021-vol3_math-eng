704d6805d1b2c307fa9723c1a4c5a30fbb869c3a
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-05-08 20:16:02 +0900

    Update Decryption/*.py

--- backend/Decryption/src/lambda_function.py
+++ backend/Decryption/src/lambda_function.py
@@ -1,8 +1,30 @@
 import json
+from . import decryption_module
 
 def lambda_handler(event: dict, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    # input
+    body = json.loads(event['body'])
+    sks = body['decKey']
+    cipher_integers = body['cipherText']
+
+    # compute
+    plain_integers = decryption_module.decrypt(cipher_integers, sks)
+
+    # output
+    if body['scheme'] is None or body['decKey'] is None or body['cipherText'] is None:
+        return {
+            'statusCode': 400
+        }
+    else:
+        print('The ciphertext has been decrypted.')
+        print('The plaintext is')
+        print(plain_integers)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(
+                {
+                    'scheme': 'rsa',
+                    'message': plain_integers
+                }
+            )
+        }
