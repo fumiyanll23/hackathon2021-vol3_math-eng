6f5c44e3a34aa2f565ee9d4690519015a299cba6
buchi1002 <kawabuchi-shinya@outlook.jp>
2021-05-09 12:35:33 +0900

    update Conversions/

--- backend/Conversion/src/hyoji.py
+++ backend/Conversion/src/hyoji.py
@@ -2,20 +2,20 @@
 ##小文字
 c = 0
 for i in range(97, 123):
-    print("'{}':'{}', " .format(c, chr(i)), end =(""))
+    print("'{}':{}, " .format(chr(i), c), end =(""))
     c += 1
 ##大文字
 for i in range(65, 91):
-    print("'{}':'{}', " .format(c, chr(i)), end =(""))
+    print("'{}':{}, " .format(chr(i), c), end =(""))
     c += 1
 ##数字
 for i in range(48, 58):
-    print("'{}':'{}', " .format(c, chr(i)), end =(""))
+    print("'{}':{}, " .format(chr(i), c), end =(""))
     c += 1
 ##ひらがな
 for i in range(ord("ぁ"), ord("ん") + 1):
     if i == ord("ぁ") or i == ord("ぃ") or i == ord("ぅ") or i == ord("ぇ") or i == ord("ぉ") or i == ord("ゃ") or i == ord("ゅ") or  i == ord("ょ") or i == ord("ゐ") or i == ord("ゑ") or i == ord("ゎ") :
         continue
-    print("'{}':'{}', " .format(c, chr(i)), end =(""))
+    print("'{}':{}, " .format(chr(i), c), end =(""))
     c += 1
 ##小文字大文字数字ひらがな
