8a19c0a14b58ffeac25bc69ea99c42c91af50490
syakoo <sakodata0318@gmail.com>
2021-05-09 11:49:21 +0900

    feat: update.

--- frontend/src/components/templates/EncForm/EncForm.tsx
+++ frontend/src/components/templates/EncForm/EncForm.tsx
@@ -1,22 +1,66 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 
 import { BtnPrim } from '@/components/atoms/Buttons'
 import { TextInput } from '@/components/atoms/TextInput'
+import { Modal } from '@/components/templates/Modal'
+import { Loading } from '@/components/atoms/Icons'
 
 import styles from './styles.module.scss'
 
 // ___________
 //
 const EncForm: React.VFC = () => {
+  const [display, setDisplay] = useState(false)
+  const [cipherText, setCipherText] = useState('')
+  const textRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = useCallback(() => {
+    if (textRef.current?.value) {
+      setDisplay(true)
+    }
+  }, [textRef, setDisplay])
+
   return (
-    <form className={styles.EncForm}>
-      <div className={styles.Input}>
-        <TextInput type="text" placeholder="メッセージ" />
+    <>
+      <div className={styles.EncForm}>
+        <div className={styles.Input}>
+          <TextInput type="text" ref={textRef} placeholder="メッセージ" />
+        </div>
+        <div className={styles.Submit}>
+          <BtnPrim type="submit" onClick={handleSubmit}>
+            暗号化
+          </BtnPrim>
+        </div>
       </div>
-      <div className={styles.Submit}>
-        <BtnPrim type="submit">暗号化</BtnPrim>
-      </div>
-    </form>
+      <Modal
+        display={display}
+        handleClose={() => {
+          setDisplay(false)
+        }}
+      >
+        {cipherText && (
+          <div>
+            <h2>{cipherText}</h2>
+            <div>
+              <BtnPrim
+                onClick={() => {
+                  setDisplay(false)
+                  setCipherText('')
+                }}
+              >
+                閉じる
+              </BtnPrim>
+            </div>
+          </div>
+        )}
+        {cipherText || (
+          <div className={styles.Loading}>
+            <Loading />
+            <div>暗号化しています...</div>
+          </div>
+        )}
+      </Modal>
+    </>
   )
 }
 
