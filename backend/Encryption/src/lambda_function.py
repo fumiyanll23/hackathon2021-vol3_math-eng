217aeea7268fbb71e93d2df2d5233bf520d2e239
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-05-09 12:21:50 +0900

    Update Encryption/

--- backend/Encryption/src/lambda_function.py
+++ backend/Encryption/src/lambda_function.py
@@ -5,26 +5,28 @@ def lambda_handler(event: dict, context):
     # input
     body = json.loads(event['body'])
     pks = body['encKey']
-    plain_integers = body['message']
+    plaintext = body['message']
 
     # compute
+    plain_integers = encryption_module.list_encode62(plaintext)
     cipher_integers = encryption_module.encrypt(plain_integers, pks)
+    ciphertext = encryption_module.list_encode_base62(cipher_integers)
 
     # output
-    if body['scheme'] is None or pks is None or plain_integers is None:
+    if body['scheme'] is None or pks is None or plaintext is None:
         return {
             'statusCode': 400
         }
     else:
         print('The plain text has been encrypted.')
         print('The ciphertext is')
-        print(cipher_integers)
+        print(ciphertext)
         return {
             'statusCode': 200,
             'body': json.dumps(
                 {
                     'scheme': 'rsa',
-                    'cipherText': cipher_integers
+                    'cipherText': ciphertext
                 }
             )
         }
